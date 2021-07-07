72ab645bbd28a1dd210dff6079f639eaf7522181
Daryl Newsholme <daryl.newsholme@questionmark.com>
2021-07-07 15:54:23 +0100

    update cert generation to generate for the hostname and the fqdn of the host

--- generate-selfsignedcert.ps1
+++ generate-selfsignedcert.ps1
@@ -1,6 +1,6 @@
 param(
     [string]$commonname = "localhost",
-    [string[]]$SAN = "localhost"
+    [string[]]$SAN = ("localhost",($env:COMPUTERNAME),"$($env:computername).$($env:userdnsdomain)")
 )
 
 push-location .\config
@@ -10,7 +10,7 @@ try {
 Catch{
 
 }
-$SAN = $SAN | ForEach-Object {"DNS:$($_)"}
+$SAN = ($SAN | ForEach-Object {"DNS:$($_)"}) -join ","
 $subject = "/CN=$commonname/emailAddress=admin@$commonname/C=UK/ST=none/L=none/O=questionmark/OU=Some Unit"
 & c:\Program` Files\OpenSSL-Win64\bin\openssl.exe req -newkey rsa:2048 -nodes -keyout vault.key -x509 -days 365 -out vault.pem -subj $subject -addext "subjectAltName = $SAN" -addext "certificatePolicies = 1.2.3.4"
 pop-location
